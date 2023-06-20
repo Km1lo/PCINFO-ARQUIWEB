18cc15abcdc7a7ed545bbb94f183a03348cd4d8a
Camilo Adama <u202117587@upc.edu.pe>
2023-06-20 01:45:41 -0500

    Usuario Agregado Frontend
    Se agrego el usuario y modificaciones en la entidad comentarios

--- src/app/component/usuario/usuario-listar/usuario-listar.component.ts
+++ src/app/component/usuario/usuario-listar/usuario-listar.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/model/usuario';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { ViewChild } from '@angular/core';
+import { UsuarioService } from 'src/app/service/usuario.service';
+import { MatDialog } from '@angular/material/dialog';
+import { UsuarioDialogoComponent } from './usuario-dialogo/usuario-dialogo.component';
 
 @Component({
   selector: 'app-usuario-listar',
   templateUrl: './usuario-listar.component.html',
   styleUrls: ['./usuario-listar.component.css']
 })
-export class UsuarioListarComponent {
+export class UsuarioListarComponent implements OnInit {
+  lista: Usuario[] = []
 
+  dataSource:MatTableDataSource<Usuario> = new MatTableDataSource();
+  idMayor: number = 0;
+
+
+  displayedColumns: string[] = ['id', 'dni', 'nombre', 'apellidop', 'apellidom', 'correo', 'sexo', 'edad', 'contrasenia', 'pais', 'accion01', 'accion2']
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+
+  constructor(private uS:UsuarioService, private dialog: MatDialog) { }
+  ngOnInit(): void {
+    this.uS.list().subscribe(data=>{
+      this.dataSource= new MatTableDataSource(data);
+        this.dataSource.paginator = this.paginator;
+    })
+    this.uS.getList().subscribe(data=>{
+
+      this.dataSource=new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator; // Asignar paginator de nuevo // RECONTRA IMPORTANTE
+
+    })
+    this.uS.getConfirmDelete().subscribe(data => {
+      data == true ? this.eliminar(this.idMayor) : false;
+    })
+  }
+
+
+  confirm(id: number) {
+    this.idMayor = id;
+    this.dialog.open(UsuarioDialogoComponent);
+  }
+  eliminar(id: number) {
+    this.uS.delete(id).subscribe(() => {
+      this.uS.list().subscribe(data => {
+        this.uS.setList(data);
+      })
+    })
+  }
+  filter(e: any) {
+    this.dataSource.filter = e.target.value.trim();
+  }
 }
--- src/app/component/usuario/usuario-registrar/usuario-registrar.component.ts
+++ src/app/component/usuario/usuario-registrar/usuario-registrar.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Usuario } from 'src/app/model/usuario';
+import * as moment from 'moment';
+import { UsuarioService } from 'src/app/service/usuario.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-registrar',
   templateUrl: './usuario-registrar.component.html',
   styleUrls: ['./usuario-registrar.component.css']
 })
-export class UsuarioRegistrarComponent {
+export class UsuarioRegistrarComponent implements OnInit {
+
+  form: FormGroup = new FormGroup({});
+  usuarioo: Usuario = new Usuario();
+  mensaje: string = "";
+  maxFecha: Date = moment().add(1, 'days').toDate();
+  id: number = 0;
+  edicion: boolean = false; //no es edicion
+  constructor(
+    private UsuarioService: UsuarioService,
+    private router: Router,
+    private route: ActivatedRoute)
+  {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id']; //capturando el id del listado
+      this.edicion = data['id'] != null;
+      this.init();
+    });
+
+    this.form = new FormGroup({
+      id: new FormControl(),
+      dni: new FormControl(),
+      nombre: new FormControl(),
+      apellidop: new FormControl(),
+      apellidom: new FormControl(),
+      correo: new FormControl(),
+      sexo: new FormControl(),
+      edad: new FormControl(),
+      contrasenia: new FormControl(),
+      pais: new FormControl()
+
+    })
+  }
+
+  init() {
+    if (this.edicion) {
+      this.UsuarioService.listId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          id: new FormControl(data.id),
+          dni: new FormControl(data.dni),
+          nombre: new FormControl(data.nombre),
+          apellidop: new FormControl(data.apellidop),
+          apellidom: new FormControl(data.apellidom),
+          correo: new FormControl(data.correo),
+          sexo: new FormControl(data.sexo),
+          edad: new FormControl(data.edad),
+          contrasenia: new FormControl(data.contrasenia),
+          pais: new FormControl(data.pais)
+        });
+      });
+    }
+  }
+
+
+
+
+  aceptar(): void {
+    this.usuarioo.id = this.form.value['id'];
+    this.usuarioo.dni = this.form.value['dni'];
+    this.usuarioo.nombre = this.form.value['nombre'];
+    this.usuarioo.apellidop = this.form.value['apellidop'];
+    this.usuarioo.apellidom = this.form.value['apellidom'];
+    this.usuarioo.correo = this.form.value['correo'];
+    this.usuarioo.sexo = this.form.value['sexo'];
+    this.usuarioo.edad = this.form.value['edad'];
+    this.usuarioo.contrasenia = this.form.value['contrasenia'];
+    this.usuarioo.pais = this.form.value['pais'];
+
+
+    if(this.form.value['contrasenia'].length > 0 && this.form.value['dni'].length > 0){
+
+      if(this.edicion){
+        this.UsuarioService.update(this.usuarioo).subscribe((data) =>
+          this.UsuarioService.list().subscribe(data=>{
+            this.UsuarioService.setList(data);
+          })
+
+        );
+      } else {
+        this.UsuarioService.insert(this.usuarioo).subscribe((data) =>
+          this.UsuarioService.list().subscribe(data=>{
+            this.UsuarioService.setList(data);
+          })
+        );
+      }
+      this.router.navigate(['administradores/mostrar/usuario']);
+    }
+    else {
+      this.mensaje = "Llene todos los campos";
+    }
+  }
+
 
 }
