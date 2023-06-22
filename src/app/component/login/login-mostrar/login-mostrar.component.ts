684b0fab6edd3e5327d2caacb1c1b1de04467033
anthc24 <86864149+anthc24@users.noreply.github.com>
2023-06-22 04:15:36 -0500

    Seguridadv6

--- src/app/component/login/login-mostrar/login-mostrar.component.ts
+++ src/app/component/login/login-mostrar/login-mostrar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { LoginService } from 'src/app/service/login.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { JwtRequest } from 'src/app/model/jwt-request';
 
 @Component({
   selector: 'app-login-mostrar',
@@ -7,16 +10,25 @@ import { Router, RouterLink } from '@angular/router';
   styleUrls: ['./login-mostrar.component.css']
 })
 export class LoginMostrarComponent implements OnInit {
+  constructor(private loginService: LoginService, private router: Router, private snackBar: MatSnackBar) { }
+  username: string = ""
+  password: string = ""
+  mensaje: string = ""
 
-  aux:boolean=true;
-  clickValues = [];
-
-  constructor (private router:Router){
-
-  }
   ngOnInit(): void {
     // Esta función se ejecutará cuando se inicialice el componente
-
+  }
+  login() {
+    let request = new JwtRequest();
+    request.username = this.username;
+    request.password = this.password;
+    this.loginService.login(request).subscribe((data: any) => {
+      sessionStorage.setItem("token", data.jwttoken);
+      this.router.navigate(['/pages/administradores/mostrar']);
+    }, error => {
+      this.mensaje = "Credenciales incorrectas!!!"
+      this.snackBar.open(this.mensaje, "Aviso",{duration:2000});
+    });
   }
 
 
