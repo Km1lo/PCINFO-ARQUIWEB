24a18dc2349a31bf91a5ba93fb3a728cfadec5db
anthc24 <86864149+anthc24@users.noreply.github.com>
2023-06-22 04:17:33 -0500

    Seguridad12

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { JwtRequest } from '../model/jwt-request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  login(request: JwtRequest) {
+    return this.http.post("http://localhost:8080/authenticate", request);
+  }
+  verificar() {
+    let token = sessionStorage.getItem("token");
+    return token != null;
+
+  }
+  showRole(){
+    let token = sessionStorage.getItem("token");
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken(token);
+    return decodedToken?.role;
+  }
 }
